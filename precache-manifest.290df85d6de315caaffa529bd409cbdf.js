d40195a0aca8c69e1f7f6b1e5e971c088aad4cc2
AlexSharavin12 <59560516+AlexSharavin12@users.noreply.github.com>
2020-01-06 17:04:51 +0500

    Add files via upload

---
+++ precache-manifest.290df85d6de315caaffa529bd409cbdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c172bf972eaf3c728e09eff25543fc65",
+    "url": "/vk-hello/index.html"
+  },
+  {
+    "revision": "ea8c911cf9b2f9692802",
+    "url": "/vk-hello/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "799e8d0bd27e6eb82b27",
+    "url": "/vk-hello/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ea8c911cf9b2f9692802",
+    "url": "/vk-hello/static/js/2.155acc8a.chunk.js"
+  },
+  {
+    "revision": "799e8d0bd27e6eb82b27",
+    "url": "/vk-hello/static/js/main.1c76b9bb.chunk.js"
+  },
+  {
+    "revision": "0b1cb88b938f0067d497",
+    "url": "/vk-hello/static/js/runtime-main.814fe65e.js"
+  }
+]);
